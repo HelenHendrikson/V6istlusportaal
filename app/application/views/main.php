b6f1413c1841bf6a78ca3a3f5d1a75e2c2d9ab71
Monkey911 <andreas.baum95@hotmail.com>
2017-03-08 12:43:47 +0200

    Mergiga tekkinud väike probleemide parandus

--- app/application/views/main.php
+++ app/application/views/main.php
@@ -10,10 +10,7 @@
 
 <div class="container">
 	<a href="<?php echo base_url(); ?>index.php/welcome/">Spordialad</a>
-<<<<<<< HEAD
 	<p><a href="<?php echo base_url(); ?>index.php/ajutine/register_form">Ajutine registreerimislink</a></p>
-=======
-	<p><a href="<?php echo base_url(); ?>index.php/ajutine/registreerimine">Ajutine registreerimislink</a></p>
 	
 	<section id="content">
 		<div class="content-layout">
@@ -33,7 +30,6 @@
 	</section>
 	
 </div>
->>>>>>> 71e53a4680d58fcab794cb8c66225cbc16dec77b
 
 
 
