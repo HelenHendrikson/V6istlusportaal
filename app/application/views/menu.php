6870f151f2a9e86b01c54aee9729f7c1e27d0e77
Helen Hendrikson <helen.hendrikson92@gmail.com>
2017-03-11 16:45:09 +0200

    mingid muudatused tehtud, bootsrapi faili kasutab nüüd ka

--- app/application/views/menu.php
+++ app/application/views/menu.php
@@ -18,6 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	 Siia saab lisada bootstrap theme ka näiteks-->
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/navbar-theme2.css"  type="text/css" />  
 		<link rel="stylesheet" href="<?php echo base_url(); ?>css/main.css"  type="text/css" /> 
+		<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css"  type="text/css" /> 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
 	
 </head>
@@ -34,7 +35,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					   <li class="all"><a href="<?php echo base_url(); ?>index.php/welcome/kkk"><?php echo $this->lang->line('voistlused');?></a></li>
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
-						<button type="button" class="btnlogin"><a href="<?php echo base_url(); ?>index.php/welcome/login"> Logi sisse </a></button>
+						<button type="button" class="btn"><a href="<?php echo base_url(); ?>index.php/welcome/login"> Logi sisse </a></button>
                         <a href="<?php echo base_url(); ?>index.php/LanguageSwitcher/switchLang/english">
                             <img src="/app/css/eng.png" id="languageimg"/>
                         </a>
--- app/application/views/searchPage.php
+++ app/application/views/searchPage.php
@@ -19,7 +19,7 @@
         function myMap() {
             var mapCanvas = document.getElementById("map");
             var myCenter = new google.maps.LatLng(58.381000,26.679878);
-            var mapOptions = {center: myCenter, zoom: 18};
+            var mapOptions = {center: myCenter, zoom: 15};
             var map = new google.maps.Map(mapCanvas,mapOptions);
             var marker = new google.maps.Marker({
                 position: myCenter,
