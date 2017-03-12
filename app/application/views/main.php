8dec12829c00c45e3807ea6266bc00f9f8cc9828
Monkey911 <andreas.baum95@hotmail.com>
2017-03-12 18:08:11 +0200

    võistluste vaade töötab taas andmebaasiga

--- app/application/views/main.php
+++ app/application/views/main.php
@@ -7,35 +7,20 @@
     <p><a href="<?php echo base_url(); ?>index.php/ajutine/register_form"><?php echo $this->lang->line('ajutine_registreerimislink');?></a></p>
 
 	<section id="content">
-<<<<<<< HEAD
-		<div class="content-layout">
-			<div class="row">
-				<div class="col" >
-				<img src="<?PHP echo base_url(); ?>images/vibu.png" alt="image not found" id="picture"/>
-				</div>
-				<div class="col" >
-				<img src="<?PHP echo base_url(); ?>images/rattasõit.png" alt="image not found" id="picture"/>
-				</div>
-				<div class="col" >
-				<img src="<?PHP echo base_url(); ?>images/ujumine.png" alt="image not found" id="picture"/>
-				</div>
-				<div class="col" ></div>
-=======
 		<div class="content-overlay">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-ms-4" >
-					<img src="<?PHP echo base_url(); ?>images/vibu.png" id="picture"/>
+					<img src="<?PHP echo base_url(); ?>images/vibu.png" alt="image not found" id="picture"/>
 					</div>
 					<div class="col-ms-4" >
-					<img src="<?PHP echo base_url(); ?>images/rattasõit.png" id="picture"/>
+					<img src="<?PHP echo base_url(); ?>images/rattasõit.png" alt="image not found" id="picture"/>
 					</div>
 					<div class="col-ms-4" >
-					<img src="<?PHP echo base_url(); ?>images/ujumine.png" id="picture"/>
+					<img src="<?PHP echo base_url(); ?>images/ujumine.png" alt="image not found" id="picture"/>
 					</div>
 					<div class="col" ></div>
 				</div>
->>>>>>> ae24d3c0865ae6cb529c08b0941842bae4472152
 			</div>
 		</div> 
 	</section>
--- app/application/views/treenerRegabSportlastvaade.php
+++ app/application/views/treenerRegabSportlastvaade.php
@@ -2,81 +2,44 @@
 
 <div class="container">
 	<section id="content">
-<<<<<<< HEAD
 		<div class="content-overlay">
 			<div class="container-fluid">
 				<div class="row" >
-					<div class="col-xs-6 col-sm-4"> 
-							<B>Vali võistlus:</B> <BR>
-							<SELECT class="form-control" name="Võistlused" size="20" MULTIPLE >
-							<OPTION SELECTED> Tartu Mai Jooks 05.05.2017
-							<OPTION> SEB Sügisjooks
-							<OPTION> U21 MM
-							<OPTION> Võistlus1
-							<OPTION> Võistlus2
-							<OPTION> Võistlus4
-							<OPTION> Võistlus3
-							</SELECT>
-					<div class="lisavoistlus">
-						<form action="<?php echo base_url(); ?>index.php/welcome/", method="input", accept-charset="UTF-8"><p><input type="text", name="voistlus"><input type="submit" value="Lisa võistlus"></p>
-						</form>
-					</div> 
-					</div>
-					<div class="col-xs-6 col-sm-4"> 
-							<B>Registreeritud sportlased:</B> <BR>
-							<SELECT class="form-control" name="Sportlased" size="20" MULTIPLE >
-							<OPTION SELECTED> Mari Rebane
-							<OPTION> i1
-							<OPTION> i2
-							<OPTION> i3
-							<OPTION> i4
-							<OPTION> i5
-							<OPTION> i6
-							</SELECT>
-						<div class="registreerisportlane" >
-							<form action="<?php echo base_url(); ?>index.php/welcome/", method="input", accept-charset="UTF-8"><p><input type="text", name="voistlus"><input type="submit" value="Registreeri sportlane"></p>
-							</form>
-						</div> 
-					</div>
-=======
-		<div class="container-fluid">
-			<div class="row" >
-				<div class="col-md-4">
-                    <form action="<?php echo base_url(); ?>index.php/welcome/voistlused" method="get" accept-charset="UTF-8">
-						<B>Tulevased võistlused:</B> <BR>
-                        <SELECT NAME="võistlused" SIZE="20" >
+					<div class="col-xs-6 col-sm-4">
+                        <div class="lisavoistlus">
+                            <form action="<?php echo base_url(); ?>index.php/welcome/voistlused" method="get" accept-charset="UTF-8">
+                                <B>Tulevased võistlused:</B> <BR>
+                                <SELECT class="form-control" name="võistlused" size="20" >
+                                    <?php if(isset($voistlused)){
+                                        foreach ($voistlused as $voistlus){ ?>
+                                            <OPTION value=<?php echo '"' . $voistlus->id . '"' ?>><?php echo $voistlus->nimi ?></OPTION>
+                                        <?php  }
+                                    } ?>
+                                </SELECT>
+                                <input type="submit" value="Vaata võistlust"></p>
+                            </form>
+                        </div>
+                        <?php if(isset($voistluse_info)) {?>
+                            <d1> <?php echo $voistluse_info[0] -> nimi; ?></d1>
+                            <p> <?php echo "distants; " . $voistluse_info[0] -> distants; ?></p>
+                            <p> <?php echo "kuupäev: " . $voistluse_info[0] -> kuupäev; ?></p>
+                        <?php } ?>
+                    </div>
 
-                            <?php if(isset($voistlused)){
-                            foreach ($voistlused as $voistlus){ ?>
-                            <OPTION value=<?php echo '"' . $voistlus->id . '"' ?>><?php echo $voistlus->nimi ?></OPTION>
-                            <?php  }
-                            } ?>
-
-						</SELECT></br>
-					<input type="submit" value="Vaata võistlust"></p>
-					</form>
-
-                    <?php if(isset($voistluse_info)) {?>
-                        <d1> <?php echo $voistluse_info[0] -> nimi; ?></d1>
-                        <p> <?php echo "distants; " . $voistluse_info[0] -> distants; ?></p>
-                        <p> <?php echo "kuupäev: " . $voistluse_info[0] -> kuupäev; ?></p>
-                    <?php } ?>
-
-				</div>
-
-				<div class="col-md-4">
-                    <?php if(isset($võistlejad)){ ?>
-                        <p>Kokku on registreerinud ennast sellele võistlusele <?php echo $count[0]->arv ?> inimest</p>
-                        <B>Registreeritud sportlased:</B> <BR>
-                        <SELECT NAME="Sportlased" SIZE="20" >
-                            <?php foreach ($võistlejad as $võistleja){ ?>
-                                <OPTION> <?php echo $võistleja-> eesnimi . " " . $võistleja-> perenimi ?></OPTION>
+					<div class="col-xs-6 col-sm-4">
+                        <div class="registreerisportlane" >
+                            <?php if(isset($võistlejad)){ ?>
+                                <p>Kokku on registreerinud ennast sellele võistlusele <?php echo $count[0]->arv ?> inimest</p>
+                                <B>Registreeritud sportlased:</B> <BR>
+                                <SELECT class="form-control" name="Sportlased" size="20" MULTIPLE >
+                                    <?php foreach ($võistlejad as $võistleja){ ?>
+                                        <OPTION> <?php echo $võistleja-> eesnimi . " " . $võistleja-> perenimi ?></OPTION>
+                                    <?php  } ?>
+                                </SELECT>
                             <?php  } ?>
-                        </SELECT>
-                    <?php } ?>
->>>>>>> 6aec5bc1ce1d400aefb4ad756e0115b71e80289e
+                        </div>
+                    </div>
 				</div>
 			</div>
-		
 	</section> 
 </div>
