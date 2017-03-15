663848e4023a7d5ebb30b71abec0e7ef4b23b078
Helen Hendrikson <helen.hendrikson92@gmail.com>
2017-03-15 19:39:54 +0200

    valideeritud töötav uus versioon

--- app/application/views/menu.php
+++ app/application/views/menu.php
@@ -21,12 +21,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     ?>
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <!-- Optional theme -->
+    <!-- Optional theme 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-	<link rel="stylesheet" href="<?php echo base_url(); ?>css/menu.css"  type="text/css" />  
+	-->
+
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css"  type="text/css" />  
 
 	<link rel="stylesheet" href="<?php echo base_url(); ?>css/main.css"  type="text/css" /> 
 	
--- app/application/views/searchPage.php
+++ app/application/views/searchPage.php
@@ -1,7 +1,7 @@
 
 <div class="container" id="content">
     <form action="<?php echo base_url(); ?>index.php/welcome/otsing" method="get" accept-charset="UTF-8">
-        <p><input type="text", name="keyword">
+        <p><input type="text" name="keyword">
             <input type="submit" value=<?php echo $this->lang->line('otsi_nupp')?>></p>
     </form>
     
