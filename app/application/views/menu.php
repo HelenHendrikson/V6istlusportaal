b7c5ed6606653913e5fe9738c7b2d8ef67cac59d
Helen Hendrikson <helen.hendrikson92@gmail.com>
2017-03-23 15:15:45 +0200

    Popup-d peaks nüüd avanema väikse aknaga ka

--- app/application/views/menu.php
+++ app/application/views/menu.php
@@ -77,11 +77,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<ul class="nav navbar-nav navbar-right">
 						
 						<li> 
-							<button id="login-toggle" type="button" class="btn btn-default navbar-button pull-right" data-toggle="modal" data-modal-id="login-modal" data-target="#login-modal" data-toggle="tooltip" data-placement="bottom" data-original-title="login" >Logi sisse</button>	
+							<button id="login-toggle" type="button" class="btn btn-default navbar-button" data-toggle="modal" data-modal-id="login-modal" data-target="#login-modal" data-toggle="tooltip" data-placement="bottom" data-original-title="login" >Logi sisse</button>	
 						</li>
 							
 						<li>
-							<div class="g-signin2 pull-right" data-onsuccess="onSignIn" data-theme="dark"></div>
+							<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
 						</li>
 
 						<li>
