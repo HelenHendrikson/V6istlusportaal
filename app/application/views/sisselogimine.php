961183126fb763f9d83c2df937350298eb47f4a3
Helen Hendrikson <helen.hendrikson92@gmail.com>
2017-03-26 15:50:05 +0300

    Tegin registreerimisele ikka ka html-i  ja showLogin ja showRegister funktsioonid, et vahetada vaateid

--- app/application/views/sisselogimine.php
+++ app/application/views/sisselogimine.php
@@ -1,32 +1,69 @@
-
-
 <!--- viide: http://bootsnipp.com/snippets/featured/clean-modal-login-form -->
+<div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="loginmodal-container" id="login-container">
+            
+            <div class="login-panel" id="login-panel">
+                <div class="panel-header">
+                    <h2 class="modal-title text-center">Logi sisse</h2>
+                    <br>
+                </div>
+                <form action="<?php echo base_url();?>index.php/login" method="post" accept-charset="UTF-8" class="form" id="login-form">
+                    <div class="form-group">
+                        <label for="username">Kasutajanimi</label>
+                        <input class="form-control" id="username" type="text" name="user" placeholder="kasutajanimi">
+                    </div>
 
+                    <div class="form-group">
+                        <label for="password">Parool</label>
+                        <input class="form-control" id="password" type="password" name="pass" placeholder="parool">
+                    </div>
 
-<div class="modal fade" id="login-modal" tabindex="-1" role="dialog"  aria-hidden="true">
-    	 <div class="modal-dialog">
-				<div class="loginmodal-container" id="login-container"> 
-					<div class="login-panel" id="login-panel" >
-						<div class="panel-header">
-							<h2 class="modal-title text-center" >Logi sisse</h2><br>
-						</div>
-					  <form action=<?php echo base_url();?>index.php/login method="post" accept-charset="UTF-8" class="login-form" id="login-form">
-						<label for="username" >Kasutajanimi</label>
-						<input class="form control" id="username" type="text" name="user" placeholder="kasutajanimi">
-						<label for="password">Parool</label>
-						<input class="form control" id="password" type="password" name="pass" placeholder="parool">
-						<label for="remember">Hoia parool meeles</label>
-						<input class="form control" id="remember" type="checkbox" checked="checked">  
-						<input class="form-control" type="submit" name="login" value="Logi sisse">
-						<input class="form control" type="button" id="contact-submit" name="register" value="Registreeru">
-					  </form>
-					
-					  <div class="login-help">
-						<a href="#">Unustasin parooli</a>
-					  </div>
-					</div> 
-				</div>
-		</div>	
-	</div>
-	
+                    <div class="form-group">
+                        <label for="remember">Hoia parool meeles</label>
+                        <input  id="remember" type="checkbox" checked="checked">
+                    </div>
 
+                    <input class="pull-left btn btn-default" onclick="showRegister()" type="button"  name="register" value="Registreeru">
+                    <input class="pull-right btn btn-primary" type="submit" name="login" value="Logi sisse">
+                </form>
+
+                <div class="row">
+                    <div class="col-xs-12 text-center">
+                        <div class="login-help">
+                            <a href="#">Unustasin parooli</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="register-panel" id="register-panel">
+                <div class="panel-header">
+                    <h2 class="modal-title text-center">Registreerimine</h2>
+                </div>
+                <div class="row">
+                    <div class="col-xs-12" id="heading"></div>
+                </div>
+                <form method="post" accept-charset="UTF-8" class="form" id="reg-form">
+					<label for="register-username">Kasutajanimi</label>
+                    <input type="text" id="register-username" placeholder="Kasutajanimi" id="username" data-toggle="tooltip" data-original-title="Sisesta kasutajanimi" data-placement="right">
+					<label for="register-name">Nimi</label>
+                    <input type="text" id="register-name" placeholder="Nimi" data-toggle="tooltip" data-original-title="Sisesta eesnimi" data-placement="right">
+					<label for="register-last-name">Perekonnanimi</label>
+                    <input type="text" id="register-last-name" placeholder="Perekonnanimi" data-toggle="tooltip" data-original-title="Sisesta perekonnanimi" data-placement="right">
+					<label for="register-maril">Meiliaadress</label>
+                    <input type="text" id="register-mail" placeholder="Meiliaadress" data-toggle="tooltip" data-original-title="Sisesta meiliaadress" data-placement="right">
+					<label for="register-password">Parool</label>
+                    <input type="password" id="register-password" placeholder="Parool" data-toggle="tooltip" data-original-title="Sisesta vähemalt 6-täheline parool" data-placement="right">
+					<label for="register-password-repeat">Parooli kinnitus</label>
+                    <input type="password" id="register-password-repeat" placeholder="Parooli kinnitus" data-toggle="tooltip" data-original-title="Sisesta parool uuesti" data-placement="right">
+
+                    <input type="button" class="pull-right btn btn-primary" onclick="validate()" value="Registreeru" id="register-button">
+
+                    <input type="button" class="pull-left btn btn-default" onclick="showLogin()" value="Logi sisse">
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- app/js/registreerimine.js
+++ app/js/registreerimine.js
@@ -1,185 +1,94 @@
-
-
 $(document).ready(function(){
 
-	$("#contact-submit").click(function(e){
-		var sisu = document.getElementById("login-panel");
-		$("#login-panel").remove();
-		
-		//Heading
+	$("input[data-toggle=\"tooltip\"]").tooltip();   
+	$("#register-panel").hide();
 
+	window.showRegister = function() {
+		$("#register-panel").show();
+		$("#login-panel").hide();
+	}
 
-		
-		
-		var heading = document.createElement('div');
-		heading.setAttribute('id',"head");
-		heading.className="panel-heading";
-		var h2 = document.createElement("h2");
-		h2.className="modal-title";
-		h2.setAttribute('id','modal-title');	
-		var t = document.createTextNode("Registreerimine"); 
-		h2.appendChild(t);
-		heading.appendChild(h2);
-		
-		var backbutton = document.createElement('button');
-		var e = document.createTextNode("Tagasi");
-		backbutton.appendChild(e);
-		backbutton.addEventListener('click',goback);
-		heading.appendChild(backbutton);
-		
-		
-		document.getElementById("login-container").append(heading);
-		
-		
-		//Form
-		
-		var form = document.createElement("form");
-        form.setAttribute('class',"form control");
-		form.setAttribute('id',"reg-form");
-
-
-        var username = document.createElement("input");
-		username.setAttribute('type',"text");
-		username.setAttribute('placeholder','kasutajanimi');
-		username.setAttribute('id','username');
-		username.setAttribute('data-toggle','tooltip');
-		username.setAttribute('data-original-title','sisesta kasutajanimi');
-		username.setAttribute('data-placement','right');
-		
-
-		var name = document.createElement("input"); 
-		name.setAttribute('type',"text");
-		name.setAttribute('placeholder','nimi');
-		name.setAttribute('data-toggle','tooltip');
-		name.setAttribute('data-original-title','Sisesta eesnimi');
-		name.setAttribute('data-placement','right');
-		
-		
-		var lastname = document.createElement("input"); 
-		lastname.setAttribute('type',"text");
-		lastname.setAttribute('placeholder','perenimi');
-		lastname.setAttribute('data-toggle','tooltip');
-		lastname.setAttribute('data-original-title','Sisesta perekonnanimi');
-		lastname.setAttribute('data-placement','right');
-		
-		var meil = document.createElement("input"); 
-		meil.setAttribute('type',"text");
-		meil.setAttribute('placeholder','meiliaadress');
-		meil.setAttribute('data-toggle','tooltip');
-		meil.setAttribute('data-original-title','Sisesta meiliaadress');
-		meil.setAttribute('data-placement','right');
-		
-		var password = document.createElement("input");
-		password.setAttribute('type','password');
-		password.setAttribute('placeholder','parool');
-		password.setAttribute('data-toggle','tooltip');
-		password.setAttribute('data-original-title','Sisesta vähemalt 6-täheline parool');
-		password.setAttribute('data-placement','right');
-		
-		var passwordRepeat = document.createElement("input");
-		passwordRepeat.setAttribute('type','password');
-		passwordRepeat.setAttribute('placeholder','parooli kinnitus');
-		passwordRepeat.setAttribute('data-toggle','tooltip');
-		passwordRepeat.setAttribute('data-original-title','Sisesta parool uuesti');
-		passwordRepeat.setAttribute('data-placement','right');
-		
-		var registerbutton = document.createElement("button");
-		registerbutton.setAttribute('type','button');
-		registerbutton.setAttribute('id','register');
-		registerbutton.addEventListener('click', validate);
-		registerbutton.innerHTML = "Registreeru";
-		
-		form.appendChild(username);
-		form.appendChild(name);
-		form.appendChild(lastname);
-		form.appendChild(meil);
-		form.appendChild(password);
-		form.appendChild(passwordRepeat);
-		form.appendChild(registerbutton);
+	window.showLogin = function() {
+		$("#register-panel").hide();
+		$("#login-panel").show();
+	}
 	
-		document.getElementById("login-container").append(form);
-		
-		
-		$("input").tooltip();   
-		
-		
-		
-		function goback(){
-			document.getElementById("reg-form").remove();
-			document.getElementById("head").remove();
-			document.getElementById("login-container").append(sisu);
-			
-			
-		};
-		
-		
-		function validate(){
-			var messages = [];
-			var valid = true;
+	window.validate = function() {
 
-			if ((username.value).length < 3 || (username.value).length > 30) {
-                messages.push("Sisesta palun 3-30 täheline kasutajanimi");
-                valid = false;
-            }
-			
-			if ((name.value).length < 2 || (name.value).length > 30) {
-				messages.push("Eeldatatud nime pikkus on 2-30 tähemärki");
-				valid = false;
-			}
+		var heading = document.getElementById("heading");
+  		var username = document.getElementById("register-username");
+		var name = document.getElementById("register-name");
+		var lastname = document.getElementById("register-last-name"); 
+		var meil = document.getElementById("register-mail"); 
+		var password = document.getElementById("register-password");
+		var passwordRepeat = document.getElementById("register-password-repeat");
 
-            if ((lastname.value).length < 2 || (lastname.value).length > 30) {
-                messages.push("Eeldatatud perekonnanime pikkus on 2-30 tähemärki");
-                valid = false;
-            }
+		var messages = [];
+		var valid = true;
 
-            if ((meil.value).length > 100) {
-                messages.push("Lubatud meili pikkus on kuni 100 tähemärki");
-                valid = false;
-            } else if (! validateEmail(meil.value)) {
-				messages.push("mittesobiv email");
-				valid = false;
-			}
-
-
-			if(password.value.length < 6 || password.value.length > 256){
-				messages.push("paaroli pikkus peab olema 5-256 tähemärki");
-				valid = false;
-			}
-			
-			if(password.value != passwordRepeat.value){
-				messages.push("paroolid ei kattu");
-				valid = false;
-			}
-			if (valid) {
-                $.ajax({
-                    type: "POST",
-                    url: "/app/index.php/login/registration",
-                    data: {"username" : username.value,
-                    		"firstname" : name.value,
-							"lastname" : lastname.value,
-                    		"meil" : meil.value,
-							"password" : password.value},
-                    dataType:'xml',
-                    success: function(data) {
-                        console.log("info saadetud");
-                        console.log(data);
-                        var $xml = $(data);
-                        var outcome = $xml.find('outcome');
-                        outcome = outcome.text();
-                        console.log(outcome);
-                        if (outcome == "success") messages.push("Registreerimine õnnestus");
-                        else if (outcome == "failed") messages.pseudo("Server ei aktsepteerinud teie sisestatud andmeid");
-                        else messages.push("Te ei läbinud xss tõrjet");
-                        console.log(messages);
-                        showMessages(messages, heading);
-                    }, error: function (data) {
-						console.log("error");
-                    }
-				})
-			}
-			showMessages(messages, heading);
+		if ((username.value).length < 3 || (username.value).length > 30) {
+            messages.push("Sisesta palun 3-30 täheline kasutajanimi");
+            valid = false;
+        }
+		
+		if ((name.value).length < 2 || (name.value).length > 30) {
+			messages.push("Eeldatatud nime pikkus on 2-30 tähemärki");
+			valid = false;
 		}
-	});
+
+        if ((lastname.value).length < 2 || (lastname.value).length > 30) {
+            messages.push("Eeldatatud perekonnanime pikkus on 2-30 tähemärki");
+            valid = false;
+        }
+
+        if ((meil.value).length > 100) {
+            messages.push("Lubatud meili pikkus on kuni 100 tähemärki");
+            valid = false;
+        } else if (! validateEmail(meil.value)) {
+			messages.push("mittesobiv email");
+			valid = false;
+		}
+
+
+		if(password.value.length < 6 || password.value.length > 256){
+			messages.push("paaroli pikkus peab olema 5-256 tähemärki");
+			valid = false;
+		}
+		
+		if(password.value != passwordRepeat.value){
+			messages.push("paroolid ei kattu");
+			valid = false;
+		}
+		if (valid) {
+            $.ajax({
+                type: "POST",
+                url: "index.php/login/registration",  // andreasel oli siin /app ka veel ees
+                data: {"username" : username.value,
+                		"firstname" : name.value,
+						"lastname" : lastname.value,
+                		"meil" : meil.value,
+						"password" : password.value},
+                dataType:'xml',
+                success: function(data) {
+                    console.log("info saadetud");
+                    console.log(data);
+                    var $xml = $(data);
+                    var outcome = $xml.find('outcome');
+                    outcome = outcome.text();
+                    console.log(outcome);
+                    if (outcome == "success") messages.push("Registreerimine õnnestus");
+                    else if (outcome == "failed") messages.pseudo("Server ei aktsepteerinud teie sisestatud andmeid");
+                    else messages.push("Te ei läbinud xss tõrjet");
+                    console.log(messages);
+                    showMessages(messages, heading);
+                }, error: function (data) {
+					console.log("error");
+                }
+			})
+		}
+		showMessages(messages, heading);
+	}	
+
 });
 
 function validateEmail(email) {
