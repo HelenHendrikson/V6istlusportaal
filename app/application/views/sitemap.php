fc8d49f5ad30f4e48ee28fd7f74396f8c5e0e17c
Helen Hendrikson <helen.hendrikson92@gmail.com>
2017-04-23 13:39:59 +0300

    lisan sitemapi otsingu

--- app/application/views/sitemap.php
+++ app/application/views/sitemap.php
@@ -7,9 +7,16 @@
 			<h3>
 				<a class="footerText" href="/index.php/welcome/" >Spordialad</a>
 			</h3>
+			<?php if ($this->session->userdata("sports_id") != NULL) {?>
+				<h3>
+                    <a class="footerText" href="<?php echo base_url(); ?>index.php/welcome/otsing"><?php echo $this->lang->line('otsing');?></a>
+				</h3>
+			<?php } ?>
+		
 			<h3>
 				<a class="footerText" href="/index.php/welcome/annetused" >Anneta</a>
 			</h3>
+			
 		</div>	
 	</div>
 </div>
