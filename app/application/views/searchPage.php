7d42e12428037245d4df8d02f5c30ad7019cd51d
Helen Hendrikson <helen.hendrikson92@gmail.com>
2017-05-21 13:47:15 +0300

    nupu stiili muutus

--- app/application/views/searchPage.php
+++ app/application/views/searchPage.php
@@ -3,7 +3,7 @@
     <form action="<?php echo base_url(); ?>index.php/welcome/otsing" method="post" accept-charset="UTF-8">
         <p><input type="text" name="keyword">
 
-            <input type="submit" value=<?php echo $this->lang->line('otsi_nupp')?>></p>
+            <input type="submit" class="btn btn-default btn-special btn2" value=<?php echo $this->lang->line('otsi_nupp')?>></p>
     </form>
     <?php if(isset($worked)){ ?>
         <form action="<?php echo base_url(); ?>index.php/welcome/otsing" method="post" accept-charset="UTF-8">
